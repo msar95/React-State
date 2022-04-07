fd559ef4dc20d13a815869ca658888fb40131e26
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-07 11:28:13 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import { App } from "./app/App";
+
+const entryPoint = document.getElementById("react-application");
+
+ReactDOM.render(<App/>,entryPoint);
