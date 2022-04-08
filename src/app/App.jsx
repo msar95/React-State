49412818f80a694e401b98604db44b6b9070d2f5
Marius Albeser <68159866+Sletger@users.noreply.github.com>
2022-04-08 20:20:43 +0200

    Used props pipeline to pass input from outside components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { ImageChanger } from "./ImageChanger";
 
+const images = [
+    {
+        src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flivewallp.com%2Fwp-content%2Fuploads%2F2021%2F07%2FOne-Piece-Zoro.jpg&f=1&nofb=1',
+        alt: 'Zoro in Wano Kuni'
+    },
+    {
+        src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2793401.jpg&f=1&nofb=1',
+        alt: 'Zoro in der neuen Welt'
+    },
+    {
+        src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F5d%2F3f%2F5b%2F5d3f5bd94b6d017a7e900dfe95790d7a--zoro-roronoa-one-piece-anime.jpg&f=1&nofb=1',
+        alt: 'Zoro auf der Grand Line'
+    }
+]
+
+
 export class App extends React.Component {
     render() {
         return (
             <div>
-                <ImageChanger />
+                <ImageChanger images = {images} interval = {3000} />
             </div>
         )
     };
--- src/app/ImageChanger.jsx
+++ src/app/ImageChanger.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 
-const images = [
-    {
-        src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flivewallp.com%2Fwp-content%2Fuploads%2F2021%2F07%2FOne-Piece-Zoro.jpg&f=1&nofb=1',
-        alt: 'Zoro in Wano Kuni'
-    },
-    {
-        src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2793401.jpg&f=1&nofb=1',
-        alt: 'Zoro in der neuen Welt'
-    },
-    {
-        src: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F5d%2F3f%2F5b%2F5d3f5bd94b6d017a7e900dfe95790d7a--zoro-roronoa-one-piece-anime.jpg&f=1&nofb=1',
-        alt: 'Zoro auf der Grand Line'
-    }
-]
 
 const getNextIndex = (images, currentIndex) => {
+ 
     if(currentIndex === images.length - 1 ){
         return 0;
     }
@@ -31,7 +18,7 @@ export class ImageChanger extends React.Component {
     componentDidMount(){
         this.intervalId = setInterval(() => {
             this.changeImage();
-         }, 10000)
+         }, this.props.interval)
     }
 
     componentWillUnmount(){
@@ -41,14 +28,14 @@ export class ImageChanger extends React.Component {
     changeImage(){
         this.setState((state, props) => {
             return{
-                imageIndex: getNextIndex(images, state.imageIndex)
+                imageIndex: getNextIndex(props.images, state.imageIndex)
             }
         }
         )
     }
 
     render() {
-        const currentimage = images[this.state.imageIndex];
+        const currentimage = this.props.images[this.state.imageIndex];
         return (
             <img src={currentimage.src} alt={currentimage.alt}></img>
         );
